1486a5e2741a78e349f8254b4daae4413f9f37a9
Anirudh Bharadwaj <69713974+Anicosmos@users.noreply.github.com>
2026-02-13 06:24:29 +0000

    implemented relu , sigmoid and softmax , yet to do tanh

--- src/ann/activations.py
+++ src/ann/activations.py
@@ -2,3 +2,18 @@
 Activation Functions and Their Derivatives
 Implements: ReLU, Sigmoid, Tanh, Softmax
 """
+import numpy as np
+
+def relu(z): ## most used activation function in deep learning
+    activation = np.maximum(0,z)
+    return activation
+def sigmoid(z): ## mainly used in binary classification problems 2B Module Reference 
+    activation = 1/(1+np.exp(-1*z))
+    return activation
+
+def softmax(z): ## mainly used in multi-class classification problems 2B Module Reference 
+    exp_z = np.exp(z - np.max(z)) 
+    activation = exp_z / np.sum(exp_z)
+    return activation
+
+#TODO: Tanh 
