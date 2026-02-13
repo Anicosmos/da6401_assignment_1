dd0d007ed459b98996a543d85bb19fbfe7189dae
Anirudh Bharadwaj <69713974+Anicosmos@users.noreply.github.com>
2026-02-13 06:24:51 +0000

    implemented the loss functions

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -2,3 +2,13 @@
 Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
+import numpy as np
+def mse(y_true, y_pred):
+    loss = np.mean((y_true-y_pred)**2)
+    return loss 
+def cross_entropy(y_true,y_pred,multiclass=False):
+    if multiclass :
+        loss = -1 * np.sum(y_true * np.log(y_pred))
+    else : ## Binary Cross Entropy ( M-ulti Label ( Independent Classifications))
+        loss = (-1/len(y_true)) * np.sum(y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))
+    return loss
