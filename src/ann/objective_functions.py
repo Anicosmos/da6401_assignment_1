84612b1d32d8d78de82cf25acd0ab0dd9741481b
Anirudh Bharadwaj <69713974+Anicosmos@users.noreply.github.com>
2026-02-17 09:02:35 +0000

    implemented objective functions derivatives

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -6,9 +6,18 @@ import numpy as np
 def mse(y_true, y_pred):
     loss = np.mean((y_true-y_pred)**2)
     return loss 
-def cross_entropy(y_true,y_pred,multiclass=False):
+def cross_entropy(y_true,y_pred,multiclass=True):
     if multiclass :
         loss = -1 * np.sum(y_true * np.log(y_pred))
-    else : ## Binary Cross Entropy ( M-ulti Label ( Independent Classifications))
+    else : ## Binary Cross Entropy
         loss = (-1/len(y_true)) * np.sum(y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))
     return loss
+def mse_derivative(y_true, y_pred):
+    grad = 2*(y_pred-y_true)/len(y_true)
+    return grad 
+def cross_entropy_derivative(y_true, y_pred,multiclass=True):
+    if multiclass :
+        grad = -1 * (y_true / y_pred)
+    else : ## done in class simplified expression for derivative 
+        grad =  (y_pred-y_true)/(y_pred*(1-y_pred))
+    return grad
