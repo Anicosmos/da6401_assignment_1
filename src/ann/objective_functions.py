f944e66395a2b653224bec8a3e6523e21e601217
Anirudh Bharadwaj <69713974+Anicosmos@users.noreply.github.com>
2026-02-17 09:23:59 +0000

    added objective class , call method will be the same

--- src/ann/objective_functions.py
+++ src/ann/objective_functions.py
@@ -3,21 +3,41 @@ Loss/Objective Functions and Their Derivatives
 Implements: Cross-Entropy, Mean Squared Error (MSE)
 """
 import numpy as np
-def mse(y_true, y_pred):
-    loss = np.mean((y_true-y_pred)**2)
-    return loss 
-def cross_entropy(y_true,y_pred,multiclass=True):
-    if multiclass :
-        loss = -1 * np.sum(y_true * np.log(y_pred))
-    else : ## Binary Cross Entropy
-        loss = (-1/len(y_true)) * np.sum(y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))
-    return loss
-def mse_derivative(y_true, y_pred):
-    grad = 2*(y_pred-y_true)/len(y_true)
-    return grad 
-def cross_entropy_derivative(y_true, y_pred,multiclass=True):
-    if multiclass :
-        grad = -1 * (y_true / y_pred)
-    else : ## done in class simplified expression for derivative 
-        grad =  (y_pred-y_true)/(y_pred*(1-y_pred))
-    return grad
+class ObjectiveFunction:
+    def __init__(self, objective_type='mse'):
+        self.objective_type = objective_type
+    def loss(self,y_true,y_pred):
+        if self.objective_type == 'mse':
+            return self.mse(y_true,y_pred)
+        elif self.objective_type == 'cross_entropy':
+            return self.cross_entropy(y_true,y_pred)
+        else:
+            # print("Unsupported, unknown obj function type")
+            raise ValueError("Unsupported")
+    def derivative(self,y_true,y_pred):
+        if self.objective_type == 'mse':
+            return self.mse_derivative(y_true,y_pred)
+        elif self.objective_type == 'cross_entropy':
+            return self.cross_entropy_derivative(y_true,y_pred)
+        else:
+            # print("Unsupported, unknown obj function type")
+            raise ValueError("Unsupported")
+        
+    def mse(self,y_true, y_pred):
+        loss = np.mean((y_true-y_pred)**2)
+        return loss 
+    def cross_entropy(self,y_true,y_pred,multiclass=True):
+        if multiclass :
+            loss = -1 * np.sum(y_true * np.log(y_pred))
+        else : ## Binary Cross Entropy
+            loss = (-1/len(y_true)) * np.sum(y_true * np.log(y_pred) + (1-y_true)*np.log(1-y_pred))
+        return loss
+    def mse_derivative(self,y_true, y_pred):
+        grad = 2*(y_pred-y_true)/len(y_true)
+        return grad 
+    def cross_entropy_derivative(self,y_true, y_pred,multiclass=True):
+        if multiclass :
+            grad = -1 * (y_true / y_pred)
+        else : ## done in class simplified expression for derivative 
+            grad =  (y_pred-y_true)/(y_pred*(1-y_pred))
+        return grad
