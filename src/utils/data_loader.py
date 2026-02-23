e6b3326152123ed411d3c58cfa91744e1be9521a
Anirudh Bharadwaj <69713974+Anicosmos@users.noreply.github.com>
2026-02-23 15:29:14 +0000

    updated Data Loaders

--- src/utils/data_loader.py
+++ src/utils/data_loader.py
@@ -9,13 +9,30 @@ from sklearn.model_selection import train_test_split
 def load_mnist():
     mnist = fetch_openml('mnist_784', version=1,as_frame=False) ## if as_frame is false it will return an numpy array
     ### Normalizing them 
-    # X = mnist.data / 255.0 
-    # y = mnist.target.astype(int)
-    x = mnist.data
-    y = mnist.target#.astype(int)
-    return x, y
+    X = mnist.data / 255.0 
+    y = mnist.target.astype(int)
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=10000, random_state=42, stratify=y
+    )
+    X_train,X_val,y_train,y_val = train_test_split(
+        X_train_val,y_train_val,test_size=0.1,random_state=42,stratify=y_train_val
+    )
+    # x = mnist.data
+    # y = mnist.target#.astype(int)
+    return X_train,y_train,X_val,y_val,X_test,y_test
 def load_fashion_mnist():
     fashion_mnist = fetch_openml('Fashion-MNIST', version=1,as_frame=False)
-    x = fashion_mnist.data
-    y = fashion_mnist.target#.astype(int)
-    return x, y
+    # x = fashion_mnist.data
+    # y = fashion_mnist.target#.astype(int)
+    ### Normalizing them 
+    X = fashion_mnist.data / 255.0 
+    y = fashion_mnist.target.astype(int)
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=10000, random_state=42, stratify=y
+    )
+    X_train,X_val,y_train,y_val = train_test_split(
+        X_train_val,y_train_val,test_size=0.1,random_state=42,stratify=y_train_val
+    )
+    # x = mnist.data
+    # y = mnist.target#.astype(int)
+    return X_train,y_train,X_val,y_val,X_test,y_test
