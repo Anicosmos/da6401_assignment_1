18994efc6c1fd453ea842cb21dcd22311288c660
Anicosmos <anirudhcosmos@gmail.com>
2026-03-07 13:06:15 +0530

    backward pass

--- src/ann/neural_network.py
+++ src/ann/neural_network.py
@@ -176,7 +176,9 @@ class NeuralNetwork:
 
         # backprop through output layer
         delta = self.layers[-1].backward(delta)
-
+        
+        grad_W_list.insert(0, self.layers[-1].grad_W)  # Insert at beginning for correct order
+        grad_b_list.insert(0, self.layers[-1].grad_b)
         #propagate through hidden layers
         for i in reversed(range(len(self.layers) - 1)):
             # multiply by the activation derivative of layer i
